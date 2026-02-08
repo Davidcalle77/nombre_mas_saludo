d8adcee118bb0dae17b6c585d3b99b90ea24e673
Davidcalle777 <david.calle@est.iudigital.edu.co>
2026-02-08 15:19:42 -0500

    se agrego palabras,valor a pagar y pesos quincena del 1 al 15

---
+++ liquidacion_nomina.py
@@ -0,0 +1,6 @@
+# Liquidacion nomina
+horas = int(input("Ingresa las horas laboradas: \n"))
+costeHora = int(input("Valor hora: "))
+totalPago = horas * costeHora
+
+print(f"Valor a pagar: ${totalPago} pesos quincena del 1 al 15")
